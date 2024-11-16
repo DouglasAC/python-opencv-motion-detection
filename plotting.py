cd25c43b162480e1cec91c2522914548a46d8add
DouglasAC <3049786550116@ingenieria.usac.edu.gt>
2024-11-16 17:41:40 -0600

    Mejora el script de visualización en plotting.py para crear y mostrar un gráfico de movimiento con Bokeh

--- plotting.py
+++ plotting.py
@@ -2,13 +2,15 @@ from app import df
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import SingleIntervalTicker
 
+# Se crea el gráfico
 p = figure(x_axis_type='datetime', height=200, width=500, title="Gráfico de Movimiento", sizing_mode="stretch_width")
 p.yaxis.ticker = SingleIntervalTicker(interval=1)
 
-
+# Se dibujan los cuadros
 q = p.quad(left=df["Inicio"], right=df["Fin"], bottom=0, top=1, color="green")
 
-
+# Se guarda el gráfico
 output_file("Graph.html")
 
+# Se muestra el gráfico
 show(p)
