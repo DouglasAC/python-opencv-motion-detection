ed0406c7a059a0cfcc35be3066ebd13e33c2b9af
DouglasAC <3049786550116@ingenieria.usac.edu.gt>
2024-11-15 18:07:37 -0600

    Se agregaron comentarios

--- app.py
+++ app.py
@@ -7,19 +7,24 @@ video = cv2.VideoCapture(0)
 while True:
     check, frame = video.read()
 
+    # Gray es la imagen en escala de grises
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
+    # Si es la primera imagen, se guarda
     if first_frame is None:
         first_frame = gray
         continue
 
+    # Se calcula la diferencia entre la primera imagen y la actual
     delta_frame = cv2.absdiff(first_frame, gray)
     thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
+    # Se buscan los contornos de la imagen
     (cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # Se dibujan los contornos
     for contour in cnts:
         if cv2.contourArea(contour) < 1000:
             continue
@@ -27,6 +32,7 @@ while True:
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
+    # Se muestran las imagenes
     cv2.imshow("Gray Frame", gray)
     cv2.imshow("Delta Frame", delta_frame)
     cv2.imshow("Threshold Frame", thresh_frame)
